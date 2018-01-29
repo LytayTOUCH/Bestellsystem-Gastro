c147e57d673bd967354d1d8aa4a3cdb3658ef5b4
Dennis Heinrich <dennis@cloudmaker97.de>
2018-01-29 04:06:06 +0100

    Update-Prozess implementiert

---
+++ app/Console/Commands/UpdateCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class UpdateCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'update:start';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Dieser Befehl ermöglicht das Aktualisieren des Bestellsystems.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        echo "Update Prozess wird durchgeführt...\n";
+
+        shell_exec('(cd '. base_path() .' && git pull -f -q && composer update && php artisan migrate)');
+
+        echo "Der Update Prozess wurde abgeschlossen!";
+    }
+}
