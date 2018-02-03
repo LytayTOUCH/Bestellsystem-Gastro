f5aa0a42b34a5636dd87a81abf94f7ba2131185b
Dennis Heinrich <dennis@cloudmaker97.de>
2018-02-03 04:56:25 +0100

    Versionsnummer angepasst

--- config/app.php
+++ config/app.php
@@ -14,7 +14,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
-    'version' => env('APP_VERSION', '0.3'),
+    'version' => env('APP_VERSION', '0.4'),
 
     /*
     |--------------------------------------------------------------------------
